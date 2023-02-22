2c6e794a155afcd4963fe739ca56fa46416d95fe
rahul <vrready4u@gmail.com>
2023-02-22 16:11:05 -0500

    logical operations

--- app.py
+++ app.py
@@ -4,3 +4,24 @@ print('bag' == 'BAG')
 
 print(ord('b'))
 print(ord('B'))
+
+
+# ternary operator
+age = 35
+
+msg = "eligble" if age > 18 else "not eligible"
+print(msg)
+
+high_income = True
+good_credit = True
+is_student = True
+
+
+if (high_income or good_credit) and not is_student:
+    print("Eligible for loan")
+else:
+    print("Not eligible")
+
+# and and or operators are short circuit in nature, that is ,in a complex boolean
+# operation with 'and' operations, the first occurance of a false, exists out the condition, becuase
+# becuase in and atleast one condition needs to be false. similar case with 'or'.
