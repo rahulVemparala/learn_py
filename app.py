6be9a4c07e6e8ec2d8d4e4cf8a41a711686ec24b
rahul <vrready4u@gmail.com>
2023-02-22 16:29:36 -0500

    loops and iterables

--- app.py
+++ app.py
@@ -25,3 +25,41 @@ else:
 # and and or operators are short circuit in nature, that is ,in a complex boolean
 # operation with 'and' operations, the first occurance of a false, exists out the condition, becuase
 # becuase in and atleast one condition needs to be false. similar case with 'or'.
+
+
+# chaining comparison operators
+
+# if age >= 18 and age < 25:
+if 18 <= age < 26:
+    print("youth")
+
+
+# loops
+# for else loop:example
+succesful = False
+
+for num in range(3):
+    if succesful:
+        print("attempt succesful , ending loop")
+        break
+
+else:
+    print("Attempted 3 times and ended, unsuccesful :( ")
+# the else block is only executed when the inner if condition fails. In this case we set the succesful flag to false, then we can see our else block execute
+
+
+# infinite loops
+# while True:
+#     command = input(">>")
+#     print(f"ECHO: {command}")
+#     if command.lower() == 'quit':
+#         break
+
+
+# iterables:
+print(type(range(4)))
+for x in range(4):
+    print(x)
+
+for ch in "python":
+    print(ch)
