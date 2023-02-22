321d3921c6f6270aaf059f90985da7a35c51ca15
rahul <vrready4u@gmail.com>
2023-02-22 15:43:49 -0500

    comparison operators concepts

---
+++ app.py
@@ -0,0 +1,6 @@
+# comparison operators:
+print('bag' == 'BAG')
+# should return a false, as numerical rep of each character is diff, even for upper and lower case letters
+
+print(ord('b'))
+print(ord('B'))
