f8900a41ec0efa1dd2d281bd23c6be1eaa3f323d
rahul <vrready4u@gmail.com>
2023-03-02 19:27:28 -0500

    Adding Numpy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # comparison operators:
+import numpy as np
 print('bag' == 'BAG')
 # should return a false, as numerical rep of each character is diff, even for upper and lower case letters
 
---
+++ numpy
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+#  numpy
+array = np.ndarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(array)
